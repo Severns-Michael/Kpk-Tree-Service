e69b9471823d9ca2093ab5b924f259ca5ebc4637
michael severns <182823702+Severns-Michael@users.noreply.github.com>
2026-08-20 22:52:59 -0600

    facebook add

--- src/data/company.ts
+++ src/data/company.ts
@@ -15,5 +15,5 @@ export const companyInfo: CompanyInfo = {
   tagline: 'Professional Tree Service & Property Maintenance',
   description:
     "Mansfield Ohio's trusted tree service and property maintenance company. From tree trimming and removal to roofing and landscaping, we handle it all.",
-  facebookUrl: 'https://www.facebook.com/kpktreeservice',
+  facebookUrl: 'https://www.facebook.com/p/KPK-Tree-Property-Maintenance-61573710742870/',
 };
